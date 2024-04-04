f4fe7aaabb7533f23eed4f1390cd11c732d6a294
jessegraham <jesse.graham.ru@gmail.com>
2024-04-04 18:14:40 -0400

    Basic user.fxml set up with UserController

---
+++ src/controller/UserController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class UserController {
+    public void logOutButton(ActionEvent actionEvent) {
+    }
+
+    public void searchButton(ActionEvent actionEvent) {
+    }
+
+    public void addAlbumButton(ActionEvent actionEvent) {
+    }
+
+    public void deleteAlbumButton(ActionEvent actionEvent) {
+    }
+
+    public void openButton(ActionEvent actionEvent) {
+    }
+
+    public void cancelSearchButton(ActionEvent actionEvent) {
+    }
+
+    public void renameButton(ActionEvent actionEvent) {
+    }
+
+    public void searchPhotosButton(ActionEvent actionEvent) {
+    }
+}
